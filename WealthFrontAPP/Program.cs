7bd1a9ae50bea15a07abc1bb6c0be9ea3d0c9e19
Telvin Roymen <tmaromeo5@gmail.com>
2022-12-06 11:07:05 +0000

    Update Program.cs

--- WealthFrontAPP/Program.cs
+++ WealthFrontAPP/Program.cs
@@ -16,7 +16,7 @@ namespace WealthFrontAPP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Home());
         }
     }
 }
